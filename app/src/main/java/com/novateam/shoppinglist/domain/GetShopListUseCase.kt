ac1b7370c8c17d0598329050b339d0e7207b7c48
novateamx <qayratkulbayev97@gmail.com>
2022-09-20 17:28:49 +0500

    created data, domain, presentation layer and them methods

---
+++ app/src/main/java/com/novateam/shoppinglist/domain/GetShopListUseCase.kt
@@ -0,0 +1,9 @@
+package com.novateam.shoppinglist.domain
+
+import androidx.lifecycle.LiveData
+
+class GetShopListUseCase(private  val shopListRepository: ShopListRepository) {
+    fun getShopList(): LiveData<List<ShopItem>> {
+        return shopListRepository.getShopList()
+    }
+}
